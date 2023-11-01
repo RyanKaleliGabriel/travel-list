d985b719609895cd390859c30a372347697ac7f1
RyanKaleliGabriel <ryang9241@gmail.com>
2023-11-01 18:48:43 +0300

    Derived state, Lifting state

---
+++ src/components/Stats.js
@@ -0,0 +1,22 @@
+export default function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding items to your packing list🚀</em>
+      </p>
+    );
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed === true).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
+  return (
+    <footer className="stats">
+      <em>
+        {percentage === 100
+          ? "You got everything ready to go✈️"
+          : `🙈 You have ${numItems} items on your list and you already packed
+        ${numPacked} (${percentage}%)`}
+      </em>
+    </footer>
+  );
+}
